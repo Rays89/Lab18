6b334745b6aae7b5e3e28cb882d8c7d08936a62a
Rays89 <aracheal795@gmail.com>
2018-05-16 19:17:40 -0400

    added a file

---
+++ Lab 18/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = { 1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10 };
+          
+
+            int max = 8;
+            foreach(int number in numbers)
+            {
+                if(number >max)
+                {
+                    max = number;
+                }
+
+            }
+
+            int[] freq = new int[max + 1];
+            for(int i = 0; i<freq.Length; i++)
+            {
+                freq[numbers[i]]++;
+            }
+            for(int i =0; i < freq.Length;i++)
+            {
+                if(freq[i] != 0)
+                {
+                    Console.WriteLine($"[{i}]: {freq[i]}");
+                }
+            }
+
+           //Racheal google = new Racheal();
+
+           // google.Add("Mike");
+           // google.Add("Anna");
+           // google.Add("Sarah");
+
+           // google.printInfo();
+           // google.RemoveAll("Mike");
+           // google.printInfo();
+        }
+    }
+}
---
+++ Lab 18/Racheal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_18
+{
+    class Racheal
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; set; }
+
+        public void Add(string userInput)//method
+        {
+            Node node = new Node { Next = null, Data = userInput };
+            if(Count == 0)
+            {
+                Head = node;
+                Tail = node;
+                Count++;
+            }
+            else
+            {
+                Tail.Next = node;
+                Tail = node;
+                Count++;
+            }
+        }
+        public void clear()
+        {
+            Count = 0;
+        }
+        public Node GetNode(int index)
+            {
+            int counter = 0;
+            Node node = Head;
+
+            while (counter != index)
+            {
+                node = node.Next;
+                counter++;//increament by 1, keep counting until we get to the index they ask for
+            }
+            return node;
+
+        }
+        public void RemoveAll(string userInput)//method
+        {
+            Node node = Head;
+            while(node.Data == userInput)
+            {
+                Head = Head.Next;
+                node = node.Next;
+                Count--;
+            }
+
+            while(node != null)
+            {
+                while(node.Next != null && node.Next.Data == userInput)
+                {
+                    node.Next = node.Next.Next;
+                    Count--;
+                }
+
+                node = node.Next;
+            }
+        }
+
+        public void printInfo()
+        {
+            Node node = Head;
+            while (node != null)
+            {
+                Console.WriteLine(node.Data);
+                node = node.Next;
+            }
+        }
+    }
+}
